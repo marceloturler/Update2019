c24752a0092a0d32dc1b7053c5b94aa7b1a319c9
marceloturler <marceloturler@hotmail.com>
2021-07-05 11:21:19 -0300

    SellerService and basica FindAll

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
@@ -40,6 +41,7 @@ namespace SalesWebMvc
                         builder.MigrationsAssembly("SalesWebMvc")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
